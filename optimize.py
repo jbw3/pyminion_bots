cd6c1fe0b069cd44ffaa9dc755b3abb807b5689f
John Wilkes <jwilkes0011@gmail.com>
2024-04-28 21:38:22 -0500

    Printing info on exception

--- optimize.py
+++ optimize.py
@@ -3,18 +3,42 @@
 from bots import OneCardBot
 from pyminion.bots.examples import BigMoney
 from pyminion.core import Card
-from pyminion.expansions.base import base_set, chapel, market, village
+from pyminion.expansions.base import base_set
 from pyminion.expansions.intrigue import intrigue_set
 from pyminion.expansions.seaside import seaside_set
 from pyminion.game import Game
 from pyminion.player import Player
-from pyminion.simulator import Simulator
+from pyminion.simulator import Simulator, SimulatorResult
+import random
 import sys
+import time
 from typing import Iterable
+
+def run_sim(players: list[Player], kingdom_cards: list[Card], iterations: int) -> SimulatorResult:
+    seed = time.time_ns() & 0xffff_ffff
+    random.seed(seed)
+
+    expansions = [base_set, intrigue_set, seaside_set]
+    game = Game(
+        players,
+        expansions,
+        kingdom_cards=kingdom_cards,
+        log_stdout=False,
+        log_file=False,
+    )
+
+    sim = Simulator(game, iterations=iterations)
+
+    try:
+        result = sim.run()
+    except:
+        print(f'seed={seed}, iterations={iterations}', file=sys.stderr)
+        raise
+
+    return result
 
 def rank_cards(cards: Iterable[Card], iterations: int) -> list[tuple[float, Card]]:
     ranked_cards: list[tuple[float, Card]] = []
-    expansions = [base_set, intrigue_set, seaside_set]
     for card in cards:
         one_card_bot = OneCardBot(card)
         big_money_bot = BigMoney()
@@ -22,15 +46,8 @@ def rank_cards(cards: Iterable[Card], iterations: int) -> list[tuple[float, Card
             one_card_bot,
             big_money_bot,
         ]
-        game = Game(
-            players,
-            expansions,
-            kingdom_cards=[card],
-            log_stdout=False,
-            log_file=False,
-        )
-        sim = Simulator(game, iterations=iterations)
-        result = sim.run()
+
+        result = run_sim(players, [card], iterations)
 
         for player_result in result.player_results:
             if player_result.player is one_card_bot:
@@ -47,7 +64,7 @@ def main() -> None:
     else:
         iterations = 100
 
-    cards = base_set[:]
+    cards = base_set + intrigue_set + seaside_set
     ranked_cards = rank_cards(cards, iterations)
     for score, card in ranked_cards:
         print(f'{score:.1%}: {card.name}')
